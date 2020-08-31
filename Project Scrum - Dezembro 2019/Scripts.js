4e30a26c68f05017a9f875c7f95526267b153da9
Ramon Maximiliano Wilbert <ramon_max1986@hotmail.com>
2020-08-30 21:38:36 -0300

    Commiting
    Commiting

---
+++ Project Scrum - Dezembro 2019/Scripts.js
@@ -0,0 +1,30 @@
+
+
+/*Dailyscrum*/
+
+function  changePicture (PngPicture) 
+      { 
+  document.getElementById ("JavaScript").src= PngPicture;
+}
+
+
+/*Dailyscrum*/
+
+
+
+
+
+
+
+/*Form*/
+
+function  calcBonus () 
+      { var bonus = parseInt(document.getElementById ('webscore').value);
+      tot = bonus * 1000;
+      document.getElementById ("bonus").value = tot;
+}
+
+/*Form*/
+
+
+
